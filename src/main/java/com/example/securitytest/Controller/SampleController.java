3f335ace71c896b55a8f1138ea2af2e15086b100
zinzo1019 <topjoy22@naver.com>
2021-11-12 22:16:38 +0900

    no message

--- src/main/java/com/example/securitytest/Controller/SampleController.java
+++ src/main/java/com/example/securitytest/Controller/SampleController.java
@@ -13,28 +13,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/sample/")
 public class SampleController {
 
-        @PreAuthorize("permitAll()") // 모든 사용자가 접근 가능
-        @GetMapping("/all")
-        public void exAll() {
-            log.info("exAll...");
-        }
+    @PreAuthorize("permitAll()") // 모든 사용자가 접근 가능
+    @GetMapping("/all")
+    public void exAll() {
+        log.info("exAll...");
+    }
 
-        @PreAuthorize("hasRole('USER')") // USER 권한이 있는 사용자만 접근 가능 (로그인 성공 시 USER)
-        @GetMapping("/user")
-        public void exUser(@AuthenticationPrincipal ClubAuthMemberDTO clubAuthMemberDTO) {
-
-            log.info("exUser...");
-
+    @PreAuthorize("hasRole('USER')") // USER 권한이 있는 사용자만 접근 가능 (로그인 성공 시 USER)
+    @GetMapping("/user")
+    public void exUser(@AuthenticationPrincipal ClubAuthMemberDTO clubAuthMemberDTO) {
+        log.info("exUser...");
     }
 
     @PreAuthorize("hasRole('MEMBER')") // MEMBER 권한이 있는 사용자만 접근 가능
     @GetMapping("/member")
     public void exMember(@AuthenticationPrincipal ClubAuthMemberDTO clubAuthMemberDTO) {
-
         log.info("exMember...");
-
-//        log.info((Supplier<String>) clubAuthMemberDTO);
-
     }
 
     @PreAuthorize("hasRole('ADMIN')") // ADMIN 권한이 있는 사용자만 접근 가능
